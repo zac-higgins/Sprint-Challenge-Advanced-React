dc41946d0702e93f0d2c4592dbc713d79c195779
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-01 10:01:08 -0600

    built class component that pulls player data from the server and renders to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({
+          players: res.data
+        })
+      })
+      .catch(err => console.log("No luck with that data", err))
+  }
+
+  // componentDidUpdate() {
+
+  // }
+
+  render() {
+    const playerList = this.state.players;
+    return (
+      <div className="App">
+        <ul>
+          {playerList.map(player => {
+            return (
+              <li key={player.id}>
+                {`${player.name}, ${player.country}, Searches: ${player.searches}`}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
